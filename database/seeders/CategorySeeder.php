56bb105a6df6ae14b44a341765b0de3c431747f0
V1niciusmv <viniciusifpe352@gmail.com>
2024-08-23 18:51:43 -0300

    add training eloquent

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        Category::create(['name' => 'Romance']);
+        Category::create(['name' => 'Fantasia']);
+        Category::create(['name' => 'Ficção Científica']);
+        Category::create(['name' => 'Ficção Brasileira']);
+        Category::create(['name' => 'Clássico']);
+        Category::create(['name' => 'Suspense']);
+    }
+}
